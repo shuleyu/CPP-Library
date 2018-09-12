598fbd3e4f6cea49fdd3516356d877b9ea88841e
Shule Yu <ysl6.626@gmail.com>
2018-09-12 15:07:27 -0700

    Add tomography tools

---
+++ Tomography.hpp
@@ -0,0 +1,28 @@
+#ifndef ASU_TOMOGRAPHY
+#define ASU_TOMOGRAPHY
+
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+
+extern "C" {
+// Current version: netcdf-4.6.1
+#include<netcdf.h>
+}
+
+/*************************************************
+ * This tool provides an interface to use the
+ * tomography nc files.
+ *
+ * Shule Yu
+ * Sept 12 2018
+ *
+ * Key words: whole mantle, tomography.
+*************************************************/
+
+namespace Tomography{
+
+}
+
+#endif
