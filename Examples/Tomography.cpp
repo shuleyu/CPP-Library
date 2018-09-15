c2923ccf95e2c82f7a9da5aa73ee0381ff1006ec
Shule Yu <ysl6.626@gmail.com>
2018-09-14 18:46:45 -0700

    sc

--- Examples/Tomography.cpp
+++ Examples/Tomography.cpp
@@ -9,17 +9,20 @@ using namespace std;
 int main(){
 
     string nc_filename="/home/shule/Research/t062.WholeMantleTomographyModels.180912/SP12RTS_dvs.nc";
-    auto model=Tomography::Model(nc_filename);
+    auto tomo=Tomography::Model(nc_filename);
 
-    // a depth section.
+    // Example 1. One value at request location (depth, lon, lat).
+//     cout << tomo.GetValueAt(2343.5,253.03,-43.4) << endl;
+
+    // Example 2. a depth slice.
     double depth=2850;
     vector<vector<double>> grid;
     double dlon=1,dlat=1,minval=numeric_limits<double>::max(),maxval=-minval;
     for (double lon=-180;lon<180; lon+=dlon)
         for (double lat=-90; lat<=90; lat+=dlat) {
-            grid.push_back({lon,lat,model.GetValueAt(depth,lon,lat)});
-//             grid.push_back({lon,lat,model.GetValueAt(depth,lon,lat)/Dvs(depth)*100-100});
-//             grid.push_back({lon,lat,model.GetValueAt(depth,lon,lat)/Dvp(depth)*100-100});
+            grid.push_back({lon,lat,tomo.GetValueAt(depth,lon,lat)});
+//             grid.push_back({lon,lat,tomo.GetValueAt(depth,lon,lat)/Dvs(depth)*100-100});
+//             grid.push_back({lon,lat,tomo.GetValueAt(depth,lon,lat)/Dvp(depth)*100-100});
             minval=min(minval,grid.back().back());
             maxval=max(maxval,grid.back().back());
         }
@@ -40,7 +43,6 @@ int main(){
 
     GMT::SealPlot(outfile);
     remove("tmp.cpt");
-
 
     return 0;
 }
